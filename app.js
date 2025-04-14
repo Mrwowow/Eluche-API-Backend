f653d1c4d428819b9345484128245f85c8ccb28c
Jacob Isah <isahjacob0@gmail.com>
2025-04-14 12:18:43 +0100

    add authRoutes

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const farmerRoutes = require('./routes/farmerRoutes');
 const processorRoutes = require('./routes/processorRoutes');
 const distributorRoutes = require('./routes/distributorRoutes');
+const authRoutes = require('./routes/authRoutes');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
@@ -10,6 +12,8 @@ app.use(express.json());
 app.use('/api/farmers', farmerRoutes);
 app.use('/api/processors', processorRoutes);
 app.use('/api/distributors', distributorRoutes);
+app.use('/api/auth', authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
