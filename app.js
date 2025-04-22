5c5cdfc80555f7572acece0faa2f6fec7d82323e
Jacob Isah <isahjacob0@gmail.com>
2025-04-22 12:16:52 +0100

    mount consumer endpoint in app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const farmerRoutes = require('./routes/farmerRoutes');
-const processorRoutes = require('./routes/processorRoutes');
-const distributorRoutes = require('./routes/distributorRoutes');
 const authRoutes = require('./routes/authRoutes');
+const consumerRoutes = require('./routes/consumerRoutes');
+
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
 app.use('/api', authRoutes); 
+app.use('/api/consumers', consumerRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
