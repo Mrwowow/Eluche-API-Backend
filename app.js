67c563fc5f7b87d43efbfbf8aa026083aa015d71
Jacob Isah <isahjacob0@gmail.com>
2025-04-26 16:10:35 +0100

    added the farm routes to app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const farmerRoutes = require('./routes/farmerRoutes');
-const authRoutes = require('./routes/authRoutes');
-const consumerRoutes = require('./routes/consumerRoutes');
+const dotenv = require('dotenv');
+const authRoutes = require('./routes/authRoutes'); 
+const consumerRoutes = require('./routes/consumerRoutes'); 
+const farmRoutes = require('./routes/farmRoutes'); 
 
-require('dotenv').config();
+dotenv.config(); 
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); 
 
-app.use('/api', authRoutes); 
+
+app.use('/api', authRoutes);
 app.use('/api/consumers', consumerRoutes);
-
+app.use('/api/farms', farmRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
