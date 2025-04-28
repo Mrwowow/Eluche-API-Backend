450a4e6961aba7ee0819c61e6d3812a7265fe0f7
Jacob Isah <isahjacob0@gmail.com>
2025-04-28 21:49:56 +0100

    added cookies parser to store the token

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const dotenv = require('dotenv');
 const authRoutes = require('./routes/authRoutes'); 
 const consumerRoutes = require('./routes/consumerRoutes'); 
 const farmRoutes = require('./routes/farmRoutes'); 
-
-dotenv.config(); 
-
+const cookieParser = require('cookie-parser');
 const app = express();
-app.use(express.json()); 
 
-
+dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+ 
 app.use('/api', authRoutes);
 app.use('/api/consumers', consumerRoutes);
 app.use('/api/farms', farmRoutes);
@@ -18,3 +18,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
